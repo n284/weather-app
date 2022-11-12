004e6ca3e395f548037050afaf323075ceddc109
n284 <n_kyo56@icloud.com>
2022-11-12 17:21:03 +0900

    version v1.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,44 +1,82 @@
-window.addEventListener("load", () => {
-    let lon;
-    let lat;
-    let temperatureDescription = this.document.querySelector("#temperature-description");
-    let temperatureDegree = this.document.querySelector("#temperature-degree");
-    let locationTimezone = this.document.querySelector("#location-timezone");
-    let wetherIcon = this.document.querySelector("#weather-icon");
+window.addEventListener("load", function() {
+    //要素取得
+    let elementLocationName = this.document.querySelector("#location_name");
+    let elementWeatherIcon = this.document.querySelector("#weather_icon");
+    let elementWeatherDescription = this.document.querySelector("#weather_description");
+    let elementTemperatureDegree = this.document.querySelector("#temperature_degree");
+    let elementMinTemperature = this.document.querySelector("#min_temperature");
+    let elementMaxTemperature = this.document.querySelector("#max_temperature");
+    let elementDataHumidity = this.document.querySelector("#data_humidity");
+    let elementDataPressure = this.document.querySelector("#data_pressure");
+    let elementDataRainfallSnowfall = this.document.querySelector("#data_rainfall_snowfall");
+    let elementDataWind = this.document.querySelector("#data_wind");
+    let elementDataCloudy = this.document.querySelector("#data_cloudy");
+    let elementDataSunriseSunset = this.document.querySelector("#data_sunrise_sunset");
+    let elementLabelRainfallSnowfall = this.document.querySelector("#label_rainfall_snowfall");
+    
+    let lon;//経度
+    let lat;//緯度
 
     if (this.navigator.geolocation) {
-        this.navigator.geolocation.getCurrentPosition(position => {
-            lon = position.coords.longitude;
-            lat = position.coords.latitude;
-            // デバッグ用
-            lon = 140.165;
-            lat = 38.508;
+        this.navigator.geolocation.getCurrentPosition(
+            (position) => {//位置情報が取得できた場合
+                lon = position.coords.longitude;
+                lat = position.coords.latitude;
+                // デバッグ用
+                // lon = 140.165;
+                // lat = 38.508;
 
-            const API_KEY = "360a9abe01d23bae800998614820c40b"
-            const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=ja`;
+                const API_KEY = "360a9abe01d23bae800998614820c40b"
+                const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=ja`;
 
+                this.fetch(URL).then((response) => {
+                    return response.json();
+                }).then((data) => {
+                    console.log(data);
+                    //データ取り出し
+                    let weatherId = data.weather[0].id;
+                    let weatherIcon = data.weather[0].icon;
+                    let weatherDescription = data.weather[0].description;
+                    let temperature = data.main.temp;
+                    let pressure = data.main.pressure;
+                    let humidity = data.main.humidity;
+                    let minTemperature = data.main.temp_min;
+                    let maxTemperature = data.main.temp_max;
+                    let speed = data.wind.speed;
+                    let windDegree = data.wind.deg;
+                    let cloudsAll = data.clouds.all;
+                    let rainOrSnow = "rain" in data ? data.rain["1h"] : "snow" in data ? data.snow["1h"] : 0;
+                    let labelRainOrSnow = "rain" in data ? "降水量" : "snow" in data ? "降雪量" : "降水量";
+                    let sunrise = data.sys.sunrise;
+                    let sunset = data.sys.sunset;
+                    let id = data.id;
+                    let name = data.name;
+                    let timezone = data.timezone;
 
+                    //書き換え
+                    elementLocationName.textContent = name;
+                    elementTemperatureDegree.textContent = temperature + " ℃";
+                    elementMinTemperature.textContent = "最低気温 : " + minTemperature + " ℃";
+                    elementMaxTemperature.textContent = "最高気温 : " + maxTemperature + " ℃";
+                    elementWeatherDescription.textContent = weatherDescription;
+                    elementDataHumidity.textContent = humidity + " %";
+                    elementDataPressure.textContent = pressure + " hPa";
+                    elementLabelRainfallSnowfall.textContent = labelRainOrSnow;
+                    elementDataRainfallSnowfall.textContent = rainOrSnow + " mm";
+                    elementDataWind.textContent = convertWindToString(speed, windDegree);
+                    elementDataCloudy.textContent = cloudsAll + " %";
+                    elementDataSunriseSunset.textContent = cnvertSunriseSunsetToString(sunrise, sunset, timezone)
+                    setIcons(weatherIcon, elementWeatherIcon, weatherId);
 
-            this.fetch(URL).then(response => {
-                return response.json();
-            }).then(data => {
-                console.log(data);
+                }).catch((fail) => {
+                    this.alert("サーバーに接続できませんでした。");
+                })
 
-                temperature = data.main.temp;
-                summary = data.weather[0].description;
-                timezone = data.sys.timezone;
-                icon = data.weather[0].icon;
-                weatherID = data.weather.id;
-
-                //Set DOM Elements from the API
-                temperatureDegree.textContent = temperature;
-                temperatureDescription.textContent = summary;
-
-
-                //setIcon
-                setIcons(icon, wetherIcon, weatherID);
-            });
-        });
+                
+            },
+            (fail) => {//位置情報が取得できない場合
+            }
+        );
     }
 
     //参考
@@ -46,7 +84,13 @@ window.addEventListener("load", () => {
     //Skyconsのアイコンの種類:https://maxdow.github.io/skycons/
     //気象庁による曇りの定義など:https://www.jma.go.jp/jma/kishou/know/yougo_hp/tenki.html
 
-    function setIcons(icon, iconID, weatherID) {
+    //getCurrentPosition:https://developer.mozilla.org/ja/docs/Web/API/Geolocation/getCurrentPosition
+    //fetch:https://breezegroup.co.jp/202004/javascript-fetch/
+    //Promiseオブジェクトによる動作フロー:https://jpdebug.com/p/3308764
+    //時計回りの角度で16方位を取得する:https://qiita.com/m-suizu@github/items/a3d69c211f8f3a5cb624
+
+
+    function setIcons(icon, element, weatherID) {
         let currentIcon;
 
         if (icon.substr(-1) === "d") {//午前
@@ -55,7 +99,11 @@ window.addEventListener("load", () => {
                     currentIcon = "CLEAR_DAY";
                     break;
 
-                case "02d", "03d"://晴れ
+                case "02d"://晴れ
+                    currentIcon = "PARTLY_CLOUDY_DAY";
+                    break;
+
+                case "03d"://晴れ
                     currentIcon = "PARTLY_CLOUDY_DAY";
                     break;
 
@@ -111,8 +159,12 @@ window.addEventListener("load", () => {
                     currentIcon = "CLEAR_NIGHT";
                     break;
 
-                case "02n", "03n"://晴れ
-                    currentIcon = "PARTLY_CLOUDY_NIGHT";
+                case "02n"://晴れ
+                    currentIcon = "PARTLY_CLOUDY_DAY";
+                    break;
+
+                case "03d"://晴れ
+                    currentIcon = "PARTLY_CLOUDY_DAY";
                     break;
 
                 case "04n"://曇り
@@ -165,6 +217,31 @@ window.addEventListener("load", () => {
 
         const skycons = new Skycons({ color: "white" });
         skycons.play();
-        skycons.set(iconID, Skycons[currentIcon]);
+        skycons.set(element, Skycons[currentIcon]);
     }
+
+    function convertUnixTimeToHourMinute(unixTime, timezone){
+        let timestamp = (unixTime + timezone)*1000;
+        let date = new Date(timestamp);
+        let formatString = date.getHours() + ":" + date.getMinutes();
+
+        return formatString;
+    }
+    function cnvertSunriseSunsetToString(sunrise, sunset, timezone){
+        let stringSunrise = convertUnixTimeToHourMinute(sunrise, timezone);
+        let stringSunset = convertUnixTimeToHourMinute(sunset, timezone);
+        let formatString = stringSunrise + "-" + stringSunset;
+
+        return formatString;
+    }
+
+    function convertWindToString(speed, degree){
+        const DIRECTION_LIST = ["北","北北東","北東", "東北東", "東", "東南東", "南東", "南南東", "南", "南南西", "南西", "西南西", "西", "西北西", "北西", "北北西", "北"];
+        let index = Math.round(degree / 22.5);//360を16で割った値が22.5
+        let formatString = DIRECTION_LIST[index] +" "+ speed + " " + "m/s";
+        
+        return formatString;
+    }
+
+
 });
